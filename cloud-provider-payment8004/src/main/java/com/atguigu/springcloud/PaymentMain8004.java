318017d29d219753fe706aa35d248037e4b12cde
r1-12king <r1-12king@outlook.com>
2022-05-22 22:31:58 +0800

    feat: 生成zookeeper服务注册中心

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/22 22:15
+ * @description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
