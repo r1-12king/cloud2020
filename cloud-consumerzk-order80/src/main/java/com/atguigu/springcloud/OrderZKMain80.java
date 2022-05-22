1eefd1129ea8667c17b78e681ccffeb8a1828158
r1-12king <r1-12king@outlook.com>
2022-05-22 22:46:18 +0800

    feat: zookeeper 消费端测试

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/22 22:41
+ * @description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
