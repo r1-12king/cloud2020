292b3a80eebcc9850b893561bc1997ffa728c67a
r1-12king <r1-12king@outlook.com>
2022-05-28 20:49:09 +0800

    feat: 1、config服务端配置

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/28 19:46
+ * @description:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
