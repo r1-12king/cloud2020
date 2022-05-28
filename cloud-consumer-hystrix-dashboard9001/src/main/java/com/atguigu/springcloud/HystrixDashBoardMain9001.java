63578099dd8b671c43824da38af58281f7c3e051
r1-12king <r1-12king@outlook.com>
2022-05-28 15:44:41 +0800

    feat: hystrixDashBoard 服务监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashBoardMain9001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/28 15:24
+ * @description:
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashBoardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashBoardMain9001.class, args);
+    }
+}
