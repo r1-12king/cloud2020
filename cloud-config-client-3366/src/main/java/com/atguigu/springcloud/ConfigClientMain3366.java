678ce3a467798256c52d76a73afef0c5a1680149
r1-12king <r1-12king@outlook.com>
2022-05-28 22:52:50 +0800

    feat: bus 增加一个新的客户端3366

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/28 22:44
+ * @description:
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -8,17 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @version 1.0
  * @auther lgl
- * @date 2022/5/28 20:55
+ * @date 2022/5/28 22:47
  * @description:
  */
 @RestController
 @RefreshScope
 public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
-    public String getConfigInfo() {
-        return configInfo;
+    public String configInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
     }
+
 }
