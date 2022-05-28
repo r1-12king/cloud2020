c34f0c8459af51faf88a1dde8e38153e41593aef
r1-12king <r1-12king@outlook.com>
2022-05-28 21:04:04 +0800

    feat: 1、Config客户端配置与测试 2、Linux运维修改GitHub上的配置文件内容做调整 3、刷新3344，发现ConfigServer配置中心立刻响应  4、刷新3355，发现ConfigClient客户端没有任何响应  5、3355没有变化除非自己重启或者重新加载  6、难到每次运维修改配置文件，客户端都需要重启？？噩梦

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/28 20:54
+ * @description:
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/5/28 20:55
+ * @description:
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
