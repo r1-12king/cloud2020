6e22ffc9c13d97ab63a26b7ae1cef4e52fb93349
r1-12king <r1-12king@outlook.com>
2022-05-29 23:39:36 +0800

    feat: sleuth
    使用zipkin 进行链路追踪

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -27,7 +27,7 @@ import java.util.List;
 @Slf4j
 public class OrderController {
 
-//    private static final String PAYMENT_URL = "http://localhost:8001";
+    //    private static final String PAYMENT_URL = "http://localhost:8001";
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
@@ -42,24 +42,24 @@ public class OrderController {
     //按理说插入数据应该是POST，可以从浏览器只能发GET请求，尽管不符合RESTFUL，
     // 但是下面的template.postForObject调用却是POST请求
     @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    public CommonResult<Payment> create(Payment payment) {
         //调用生产者模块中的生产方法
-        return restTemplate.postForObject(PAYMENT_URL+"payment/create", payment, CommonResult.class);
+        return restTemplate.postForObject(PAYMENT_URL + "payment/create", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/getForEntity/{id}")
     public CommonResult getPayment2(@PathVariable("id") Long id) {
-        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
 
-        if(entity.getStatusCode().is2xxSuccessful()){
+        if (entity.getStatusCode().is2xxSuccessful()) {
             return entity.getBody();
-        }else{
-            return new CommonResult<>(444,"操作失败");
+        } else {
+            return new CommonResult<>(444, "操作失败");
         }
     }
 
@@ -67,16 +67,21 @@ public class OrderController {
     public String getPaymentLB() {
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
 
-        if(instances == null || instances.size() <= 0)
-        {
+        if (instances == null || instances.size() <= 0) {
             return null;
         }
 
         ServiceInstance serviceInstance = loadBalancer.instances(instances);
         URI uri = serviceInstance.getUri();
 
-        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
 
+    }
+
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin/", String.class);
+        return result;
     }
 
 }
