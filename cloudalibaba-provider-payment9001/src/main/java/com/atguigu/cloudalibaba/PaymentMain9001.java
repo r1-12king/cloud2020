b82dc69181a22187593f13f88013bb12c7d20d8b
r1-12king <r1-12king@outlook.com>
2022-06-01 23:00:44 +0800

    feat: springcloud alibaba
    nacos 服务提供者创建

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/cloudalibaba/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/6/1 22:43
+ * @description:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/cloudalibaba/PaymentMain9002.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @version 1.0
+ * @auther lgl
+ * @date 2022/6/1 22:49
+ * @description:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
