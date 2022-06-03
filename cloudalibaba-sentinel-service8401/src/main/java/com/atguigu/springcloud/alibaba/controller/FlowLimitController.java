51598a511b20f2d972b2a00b8f8541693a72f7b7
r1-12king <r1-12king@outlook.com>
2022-06-03 22:22:23 +0800

    feat: springcloud alibaba sentinel降级
    热点key限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -45,5 +48,27 @@ public class FlowLimitController {
         log.info("testE 测试异常数");
         int age = 10 / 0;
         return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        // 添加异常，不管
+        /**
+         * @SentinelResource
+         * 处理的是Sentinel控制台配置的违规情况，有blockHandler方法配置的兜底处理；
+         * RuntimeException
+         * int age = 10/0,这个是java运行时报出的运行时异常RunTimeException，@SentinelResource不管
+         * 总结
+         *  @SentinelResource主管配置出错，运行出错该走异常走异常
+         */
+//        int age = 10/0;
+
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
     }
 }
